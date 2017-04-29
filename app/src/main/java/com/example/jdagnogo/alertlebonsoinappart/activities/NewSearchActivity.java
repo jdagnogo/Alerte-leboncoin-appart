cb64ad885916ab83f47a318be416ea5874cb7f94
jdagnogo11 <jdagnogo11@gmail.com>
2017-04-29 18:10:33 +0200

    create new search activity with coordinate layout

---
+++ app/src/main/java/com/example/jdagnogo/alertlebonsoinappart/activities/NewSearchActivity.java
@@ -0,0 +1,15 @@
+package com.example.jdagnogo.alertlebonsoinappart.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.jdagnogo.alertlebonsoinappart.R;
+
+public class NewSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_search);
+    }
+}
