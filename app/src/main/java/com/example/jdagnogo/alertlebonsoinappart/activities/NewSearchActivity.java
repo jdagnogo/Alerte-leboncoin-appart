3882b77e121a971c3cf41191718c4bd8cd91ef5c
jdagnogo11 <jdagnogo11@gmail.com>
2017-04-30 11:42:00 +0200

    add scrollChoice

--- app/src/main/java/com/example/jdagnogo/alertlebonsoinappart/activities/NewSearchActivity.java
+++ app/src/main/java/com/example/jdagnogo/alertlebonsoinappart/activities/NewSearchActivity.java
@@ -1,9 +1,14 @@
 package com.example.jdagnogo.alertlebonsoinappart.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.jdagnogo.alertlebonsoinappart.R;
+import com.webianks.library.scroll_choice.ScrollChoice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewSearchActivity extends AppCompatActivity {
 
@@ -11,5 +16,34 @@ public class NewSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_search);
+
+        ScrollChoice scrollChoice = (ScrollChoice) findViewById(R.id.scroll_choice);
+        final ScrollChoice scrollChoice2 = (ScrollChoice) findViewById(R.id.scroll_choice2);
+
+
+        final List<String> data = new ArrayList<>();
+        data.add("0");
+        data.add("100");
+        data.add("200");
+        data.add("300");
+        data.add("400");
+        data.add("500");
+
+        scrollChoice.addItems(data,0);
+        scrollChoice2.addItems(data,5);
+        scrollChoice.setOnItemSelectedListener(new ScrollChoice.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(ScrollChoice scrollChoice, int position, String name) {
+                Log.d("webi",name);
+
+            }
+        });
+
+        scrollChoice.setOnItemSelectedListener(new ScrollChoice.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(ScrollChoice scrollChoice, int position, String name) {
+                Log.d("webi",name);
+            }
+        });
     }
 }
