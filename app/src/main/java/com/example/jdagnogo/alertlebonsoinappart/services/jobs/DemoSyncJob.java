96f3bdd30b75ca2bd20c535504c03ece88b620c3
jdagnogo11 <jdagnogo11@gmail.com>
2017-05-12 13:03:39 +0200

    job periodic

--- app/src/main/java/com/example/jdagnogo/alertlebonsoinappart/services/jobs/DemoSyncJob.java
+++ app/src/main/java/com/example/jdagnogo/alertlebonsoinappart/services/jobs/DemoSyncJob.java
@@ -9,7 +9,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.evernote.android.job.Job;
 import com.evernote.android.job.JobRequest;
@@ -18,6 +17,7 @@ import com.example.jdagnogo.alertlebonsoinappart.R;
 import com.example.jdagnogo.alertlebonsoinappart.services.retrofit.RetrofitNetworkInterface;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -34,39 +34,23 @@ public class DemoSyncJob extends Job {
     public static final String TAG = "job_demo_tag";
     @Inject
     Retrofit retrofit;
+    String result = "";
     @Override
     @NonNull
     protected Result onRunJob(Params params) {
-        int i = 0;
-        i++;
-        Log.e("toto", "int i : " + i);
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent();
+
                 Context context = AlertLEboncoinApplication.getContext();
                 ((AlertLEboncoinApplication) context).getNetworkComponent().inject(DemoSyncJob.this);
 // use System.currentTimeMillis() to have a unique ID for the pending intent
-                PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
+
                 getAppart();
 
 // build notification
 // the addAction re-use the same intent to keep the example short
-                Notification n = new Notification.Builder(context)
-                        .setContentTitle("alerte lebon coin")
-                        .setContentText("toto")
-                        .setSmallIcon(R.drawable.test)
-                        .setContentIntent(pIntent)
-                        .setAutoCancel(true)
-                        .addAction(R.drawable.test, "Call", pIntent)
-                        .addAction(R.drawable.test, "More", pIntent)
-                        .addAction(R.drawable.test, "And more", pIntent).build();
 
-
-                NotificationManager notificationManager =
-                        (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
-
-                notificationManager.notify(0, n);
             }
         });
         return Result.SUCCESS;
@@ -74,7 +58,8 @@ public class DemoSyncJob extends Job {
 
     public static void scheduleJob() {
         new JobRequest.Builder(DemoSyncJob.TAG)
-                .setExecutionWindow(10_000L, 15_000L)
+                .setPeriodic(TimeUnit.MINUTES.toMillis(15), TimeUnit.MINUTES.toMillis(5))
+                .setPersisted(true)
                 .build()
                 .schedule();
     }
@@ -87,6 +72,25 @@ public class DemoSyncJob extends Job {
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 try {
                     Log.d(TAG, "Result " + response.body().string());
+                    result = "" +response.code();
+                    Context context = AlertLEboncoinApplication.getContext();
+                    Intent intent = new Intent();
+                    PendingIntent pIntent = PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
+                    Notification n = new Notification.Builder(context)
+                            .setContentTitle("alerte lebon coin")
+                            .setContentText(result)
+                            .setSmallIcon(R.drawable.test)
+                            .setContentIntent(pIntent)
+                            .setAutoCancel(true)
+                            .addAction(R.drawable.test, "Call", pIntent)
+                            .addAction(R.drawable.test, "More", pIntent)
+                            .addAction(R.drawable.test, "And more", pIntent).build();
+
+
+                    NotificationManager notificationManager =
+                            (NotificationManager) context.getSystemService(NOTIFICATION_SERVICE);
+
+                    notificationManager.notify(0, n);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
